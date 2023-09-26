7eb27f92a8b5c2ee7e2ceb1a2329b46405b9706a
Samuel Benediktus Meliala <brunodargo.bella@gmail.com>
2023-09-26 14:28:28 +0700

    final program

--- main.java
+++ main.java
@@ -7,16 +7,17 @@ public class main {
 
     private static int inputOperator(){
         if(firstRun){
-            System.out.print("Enter the first number");
+            System.out.print("Enter the first number: ");
             total = scanner.nextDouble();
             firstRun = false;
+            System.out.println("Enter the operation: ");
         } else{
-            System.out.println("Enter a number");
+            System.out.println("Enter the operation: ");
         }
         int operator = scanner.nextInt();
         return operator;
     }
-    
+
     private static void addition(){
         System.out.println("Enter a number to perform addition: ");
         double num = scanner.nextDouble();
@@ -29,8 +30,43 @@ public class main {
         total -= num;
     }
 
+    private static void multiply(){
+        System.out.println("Enter a number to perform multiplication: ");
+        double num = scanner.nextDouble();
+        total *= num; 
+    }
+
+    private static void divide(){
+        System.out.println("Enter a number to perform division: ");
+        double num = scanner.nextDouble();
+        if(num != 0){
+            total /= num;
+        }else{
+            System.out.println("Cannot divide by zero (0)");
+        }
+    }
+
+    private static void modulo(){
+        System.out.println("Enter a number to perform modulo: ");
+        double num = scanner.nextDouble();
+        if(num != 0){
+            total %= num;
+        }else{
+            System.out.println("Cannot do modulo by zero (0)");
+        }
+    }
+
+    private static void clear(){
+        firstRun = true;
+        total = 0.0;
+        System.out.println("Total has been cleared, input new number.");
+    }
+
     public static void main(String[] args) {
         while(true){
+            System.out.println("================================");
+            System.out.println("Basic Calculator Program");
+            System.out.println("================================");
             System.out.println("Total: " + total);
             System.out.println("1. Add");
             System.out.println("2. Subtract");
@@ -39,7 +75,6 @@ public class main {
             System.out.println("5. Modulo");
             System.out.println("6. Clear Total");
             System.out.println("7. Quit");
-            System.out.println("Choose Operator: ");
             int operator = inputOperator();
             switch (operator) {
                 case 1:
@@ -48,11 +83,25 @@ public class main {
                 case 2:
                     subtraction();
                     break;
+                case 3:
+                    multiply();
+                    break;
+                case 4:
+                    divide();
+                    break;
+                case 5:
+                    modulo();
+                    break;
+                case 6: 
+                    clear();
+                    break;
+                case 7:
+                    System.out.println("Exiting the program, thank you.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please enter the correct option!");
             }
         }
     }
-
-    
-
-    
 }
